45feff3eca8d051905573276de194eb4e1a56416
Assembly Consultoria <assemblyconsultoria@gmail.com>
2024-09-22 20:58:06 -0300

    Criado a lista de cursos

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../models/curso';
+
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  cursos: Curso[] = [
+    { _id: "1", nome: "Angular 18", categoria: "Front-End" },
+    { _id: "2", nome: "SpringBoot", categoria: "Back-End" }
+  ];
+  displayedColumns = ['nome', 'categoria'];
+
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+
+  }
 
 }
